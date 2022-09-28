6e1445af8401780f076f4eed63bca1319a3aad11
Joseph Ochoa <josephochoa91@gmail.com>
2022-09-28 12:13:06 -0700

    Make SongArtistList Render a list of SongArtistCard Components

--- components/SongArtistList/SongArtistList.jsx
+++ components/SongArtistList/SongArtistList.jsx
@@ -1,10 +1,28 @@
-import styles from './styles';
+import { useRouter } from 'next/router';
 import { useGetWorldChartsQuery } from '../../services/shazamCoreApi';
+import { SongArtistCard } from '../';
 
 const SongArtistList = () => {
-  const { data, isFetching, error } = useGetWorldChartsQuery();
+  const currentPath = useRouter().route;
+  const { data: worldChartsData, isFetching, error } = useGetWorldChartsQuery();
 
-  return <div>SongArtistList</div>;
+  const renderCorrectItems = () => {
+    if (currentPath === '/') {
+      return worldChartsData.map(({ key, title, subtitle }) => (
+        <SongArtistCard key={key} title={title} subtitle={subtitle} />
+      ));
+    }
+  };
+
+  if (isFetching) {
+    return '...Loading - Test Loader';
+  }
+
+  if (error) {
+    return 'Error - Test Error';
+  }
+
+  return <>{renderCorrectItems()}</>;
 };
 
 export default SongArtistList;
