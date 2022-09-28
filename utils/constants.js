a7755b0717ea64fc356b5af7469ae682cd340b8a
Joseph Ochoa <josephochoa91@gmail.com>
2022-09-28 15:04:19 -0700

    Add Gold Gradients

--- utils/constants.js
+++ utils/constants.js
@@ -10,4 +10,38 @@ export const COLORS = {
   greys: {
     100: 'hsl(0deg 0% 60%)',
   },
+  gradients: {
+    gold: `radial-gradient(
+      ellipse farthest-corner at right bottom,
+      hsl(49deg 99% 61%) 0%,
+      hsl(40deg 98% 59%) 8%,
+      hsl(41deg 60% 39%) 30%,
+      hsl(42deg 49% 36%) 40%,
+      transparent 80%
+    ),
+    radial-gradient(
+      ellipse farthest-corner at left top,
+      hsl(0deg 0% 100%) 0%,
+      hsl(60deg 100% 84%) 8%,
+      hsl(44deg 54% 61%) 25%,
+      hsl(42deg 50% 24%) 62.5%,
+      hsl(42deg 50% 24%) 100%
+    )`,
+    goldTransparent: `radial-gradient(
+      ellipse farthest-corner at right bottom,
+      hsl(49deg 99% 61% /0.5) 0%,
+      hsl(40deg 98% 59% /0.5) 8%,
+      hsl(41deg 60% 39% /0.5) 30%,
+      hsl(42deg 49% 36% /0.5) 40%,
+      transparent 80%
+    ),
+    radial-gradient(
+      ellipse farthest-corner at left top,
+      hsl(0deg 0% 100% /0.5) 0%,
+      hsl(60deg 100% 84% /0.5) 8%,
+      hsl(44deg 54% 61% /0.5) 25%,
+      hsl(42deg 50% 24% /0.5) 62.5%,
+      hsl(42deg 50% 24% /0.5) 100%
+    )`,
+  },
 };
