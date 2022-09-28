546b815e46c7e0b9587c13e2949e51a6e8c290e7
Joseph Ochoa <josephochoa91@gmail.com>
2022-09-28 13:33:20 -0700

    Allow images from specific domains for the Next.js Image component

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    domains: [
+      'is1-ssl.mzstatic.com',
+      'is2-ssl.mzstatic.com',
+      'is3-ssl.mzstatic.com',
+      'is4-ssl.mzstatic.com',
+      'is5-ssl.mzstatic.com',
+    ],
+  },
 };
 
 module.exports = nextConfig;
