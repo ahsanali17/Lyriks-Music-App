bb52ddcb8fc97e1986e473ad1c99559cf690ea2c
Joseph Ochoa <josephochoa91@gmail.com>
2022-09-28 12:39:16 -0700

    Change Data Fetching Mental Paradigm from Get Data by Component to Get Data by Page

--- components/MainContent/MainContent.jsx
+++ components/MainContent/MainContent.jsx
@@ -1,12 +1,22 @@
 import { MainContentWrapper } from './styles';
 import { Search, Genres, SongArtistList } from '../';
+import { useGetWorldChartsQuery } from '../../services/shazamCoreApi';
 
 const MainContent = () => {
+  const { data, isFetching, error } = useGetWorldChartsQuery();
+
+  if (isFetching) {
+    return '...Loading - Test Loader';
+  }
+
+  if (error) {
+    return 'Error - Test Error';
+  }
   return (
     <MainContentWrapper>
       <Search />
       <Genres />
-      <SongArtistList />
+      <SongArtistList data={data} />
     </MainContentWrapper>
   );
 };
--- components/SongArtistList/SongArtistList.jsx
+++ components/SongArtistList/SongArtistList.jsx
@@ -1,28 +1,13 @@
-import { useRouter } from 'next/router';
-import { useGetWorldChartsQuery } from '../../services/shazamCoreApi';
 import { SongArtistCard } from '../';
 
-const SongArtistList = () => {
-  const currentPath = useRouter().route;
-  const { data: worldChartsData, isFetching, error } = useGetWorldChartsQuery();
-
-  const renderCorrectItems = () => {
-    if (currentPath === '/') {
-      return worldChartsData.map(({ key, title, subtitle }) => (
+const SongArtistList = ({ data }) => {
+  return (
+    <>
+      {data.map(({ key, title, subtitle }) => (
         <SongArtistCard key={key} title={title} subtitle={subtitle} />
-      ));
-    }
-  };
-
-  if (isFetching) {
-    return '...Loading - Test Loader';
-  }
-
-  if (error) {
-    return 'Error - Test Error';
-  }
-
-  return <>{renderCorrectItems()}</>;
+      ))}
+    </>
+  );
 };
 
 export default SongArtistList;
