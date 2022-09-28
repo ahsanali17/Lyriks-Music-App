047b34ae71b91ec106a8d66fccd44b65c5bb1d42
Joseph Ochoa <josephochoa91@gmail.com>
2022-09-28 15:04:52 -0700

    Add Styles to SongArtistCard

--- components/SongArtistCard/styles.js
+++ components/SongArtistCard/styles.js
@@ -1,8 +1,17 @@
 import styled from 'styled-components';
 
 export const CardWrapper = styled.div`
-  height: 250px;
-  width: 220px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  height: 325px;
+  width: 250px;
+
+  background: ${(props) => props.theme.colors.gradients.goldTransparent};
+  border-radius: 20px;
 `;
 
 export const TextContainer = styled.div`
--- components/SongArtistList/styles.js
+++ components/SongArtistList/styles.js
@@ -4,7 +4,7 @@ export const CardWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(
     auto-fill,
-    minmax(0, min(100%/3, max(220px, 100%/5)))
+    minmax(0, min(100%/3, max(250px, 100%/5)))
   );
   place-content: center;
   gap: 20px;
