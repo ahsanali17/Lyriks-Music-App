d3c1095fec6f59d507b1ff7f1e5dda437f3e3564
Joseph Ochoa <josephochoa91@gmail.com>
2022-10-03 11:52:37 -0700

    Make Github Review Changes

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Head from 'next/head';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+
 import { HomeWrapper } from './styles';
 import {
   Sidebar,
   MainContent,
   TopChartsTopArtistsContainer,
 } from '../components';
-import { ThemeProvider } from 'styled-components';
 import { COLORS } from '../utils/constants';
-import { Provider } from 'react-redux';
 import store from '../app/store';
 
 const Home = () => {
