c2bbd5d99aa08c9f0831bcf92da7c8bbf1c55b74
ahsanali17 <ahsantime1@gmail.com>
2022-10-05 20:01:58 -0400

    this is a initial branch commit, to see how the explore page looks like when populated with components

--- components/Explore/Explore.jsx
+++ components/Explore/Explore.jsx
@@ -1,7 +1,25 @@
-import styles from './styles';
+import { MainContentWrapper } from './styles';
+import { Search, SongArtistList } from '..';
+import { useGetWorldChartsByGenreQuery } from '../../services/shazamCoreApi';
 
 const Explore = () => {
-  return <div>Explore</div>;
+  
+  const { data, isFetching, error } = useGetWorldChartsByGenreQuery('ROCK');
+  
+  if (isFetching) {
+    return '...Loading - Test Loader';
+  }
+
+  if (error) {
+    return 'Error - Test Error';
+  }
+  
+  return (
+    <MainContentWrapper>
+      <Search />
+      <SongArtistList data={data} />
+    </MainContentWrapper>
+  );
 };
 
 export default Explore;
