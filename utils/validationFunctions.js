90665278d1e6a09efb4cf1f912eb98570d514eaa
Joseph Ochoa <josephochoa91@gmail.com>
2022-10-08 13:28:33 -0700

    Remove Duplicate Artists

--- utils/validationFunctions.js
+++ utils/validationFunctions.js
@@ -12,19 +12,31 @@ export const returnFirst5ValidArtists = (() => {
   const artistsArr = [];
   let currentArtist;
   let currentBackgroundImage;
+  let currentArtistName;
 
   return function (worldChartsData) {
+    artistsArr.length > 1 ? (artistsArr = []) : artistsArr;
+
+    let artistNames = [];
+
     for (let i = 0; i < worldChartsData.length; i += 1) {
       currentArtist = worldChartsData[i];
+      currentArtistName = worldChartsData[i].subtitle;
       currentBackgroundImage = currentArtist?.images?.background;
 
       if (artistsArr.length === 5) {
         return artistsArr;
       }
 
-      if (currentBackgroundImage && urlSuccess(currentBackgroundImage)) {
+      if (
+        currentBackgroundImage &&
+        urlSuccess(currentBackgroundImage) &&
+        !artistNames.includes(currentArtistName)
+      ) {
         artistsArr.push(currentArtist);
       }
+
+      artistNames.push(currentArtistName);
     }
 
     return artistsArr;
