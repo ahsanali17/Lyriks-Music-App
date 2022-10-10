995b484fa61dc5281bf0ab6bfb8d289e8a021d17
Joseph Ochoa <josephochoa91@gmail.com>
2022-10-10 14:41:41 -0700

    Fix Internal vs External imports

--- components/LeftSidebar/LeftSidebar.jsx
+++ components/LeftSidebar/LeftSidebar.jsx
@@ -1,28 +1,39 @@
-import { Logo } from '..';
-import { SidebarWrapper, SidebarItemContainer, SidebarItemList, ItemIcon, ItemLabel, ItemContainer, Divider } from './styles.js';
 import Link from 'next/link';
 
+import { Logo } from '..';
+import {
+  SidebarWrapper,
+  SidebarItemContainer,
+  SidebarItemList,
+  ItemIcon,
+  ItemLabel,
+  ItemContainer,
+  Divider,
+} from './styles.js';
 import sidebarIcons from '../../assets/images/sidebarIcons/index';
 
 const linksArray = [
-  {label: 'Home', to: "/home"},
-  {label: 'Explore', to: "/explore"},
-  {label: 'Artists', to: "/artists"},
-  {label: 'Albums', to: "/albums"},
+  { label: 'Home', to: '/home' },
+  { label: 'Explore', to: '/explore' },
+  { label: 'Artists', to: '/artists' },
+  { label: 'Albums', to: '/albums' },
 ];
 
 const LeftSidebar = () => {
-  
   return (
     <SidebarWrapper>
       <Logo />
       <Divider />
-      {linksArray.map(({label, to}) => (
+      {linksArray.map(({ label, to }) => (
         <SidebarItemContainer>
           <SidebarItemList key={label}>
             <Link href={to}>
-              <ItemContainer onClick={() => {console.log('button pressed')}}>
-                <ItemIcon src={sidebarIcons[label]}/>
+              <ItemContainer
+                onClick={() => {
+                  console.log('button pressed');
+                }}
+              >
+                <ItemIcon src={sidebarIcons[label]} />
                 <ItemLabel>{label}</ItemLabel>
               </ItemContainer>
             </Link>
