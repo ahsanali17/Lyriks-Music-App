ddbc1fefeeb23224013d0242c4a2d26f3b33c605
Joseph Ochoa <josephochoa91@gmail.com>
2022-10-11 11:56:06 -0700

    Add Play styles to song card

--- components/SongArtistCard/SongArtistCard.jsx
+++ components/SongArtistCard/SongArtistCard.jsx
@@ -1,12 +1,17 @@
 import Image from 'next/image';
+import { BsFillPlayFill } from 'react-icons/bs';
 
+import { ImageWrapper } from '../../styles/resusableStyles';
 import { CardOverlay, CardWrapper, TextContainer } from './styles';
 
 const SongArtistCard = ({ coverArt, title, subtitle }) => {
   return (
     <CardOverlay>
       <CardWrapper>
-        <Image src={coverArt} width={220} height={225} objectFit="cover" />
+        <ImageWrapper>
+          <Image src={coverArt} width={220} height={225} objectFit="cover" />
+          <BsFillPlayFill />
+        </ImageWrapper>
         <TextContainer>
           <h3>{title}</h3>
           <h5>{subtitle}</h5>
--- styles/resusableStyles.js
+++ styles/resusableStyles.js
@@ -8,3 +8,30 @@ export const OverlayWrapper = styled.div`
     }
   }
 `;
+
+export const ImageWrapper = styled.div`
+  position: relative;
+
+  @media (pointer: fine) {
+    svg {
+      display: none;
+    }
+
+    &:hover {
+      img {
+        opacity: 0.75;
+      }
+
+      svg {
+        display: revert;
+
+        position: absolute;
+        inset: 0;
+        margin: auto;
+
+        width: 25%;
+        height: 25%;
+      }
+    }
+  }
+`;
