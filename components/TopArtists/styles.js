1fabbb1304e236ad9e4e0ab213e74cdca841faad
Joseph Ochoa <josephochoa91@gmail.com>
2022-10-12 20:35:13 -0700

    Add Top Artists Touchscreen Layout

--- components/TopArtists/styles.js
+++ components/TopArtists/styles.js
@@ -11,8 +11,10 @@ export const TopArtistsWrapper = styled.div`
   width: 100%;
 
   font-size: 1.5rem;
+`;
 
-  h1 {
+export const Heading = styled.h1`
+  @media (pointer: fine) {
     margin-top: 100px;
 
     background-image: ${(props) => props.theme.colors.gradients.gold};
@@ -21,6 +23,10 @@ export const TopArtistsWrapper = styled.div`
     -webkit-text-fill-color: transparent;
     -moz-background-clip: text;
     -moz-text-fill-color: transparent;
+  }
+
+  @media (pointer: coarse) {
+    display: none;
   }
 `;
 
@@ -31,6 +37,18 @@ export const ArtistCardsWrapper = styled.div`
   height: 100%;
   width: 100%;
   padding-top: 40px;
+
+  @media (pointer: coarse) {
+    display: grid;
+    grid-template-rows: repeat(4, 1fr);
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-areas:
+      'heading heading'
+      'one one'
+      'two three'
+      'four five';
+    gap: 40px;
+  }
 `;
 
 export const ArtistGradientWrapper = styled.div`
@@ -75,6 +93,8 @@ export const ArtistWrapper = styled.div`
       }
 
       h6 {
+        display: revert;
+
         overflow: hidden;
         text-overflow: ellipsis;
 
@@ -100,7 +120,7 @@ export const ArtistWrapper = styled.div`
 
           z-index: 2;
           transform: scale(1.25);
-          bottom: 20px;
+          left: 20px;
           left: 95px;
         }
       }
@@ -176,115 +196,74 @@ export const ArtistWrapper = styled.div`
     }
   }
 
-  @media (pointer: none) {
+  @media (pointer: coarse) {
     h6 {
+      display: none;
     }
-
-    &:hover {
-      img {
-        border-radius: 50%;
-        opacity: 0.5;
-      }
-
-      h6 {
-        overflow: hidden;
-        text-overflow: ellipsis;
-
-        padding: 20px;
-        display: revert;
-        max-width: max-content;
-        max-height: max-content;
-        position: absolute;
-      }
-    }
-
     :nth-of-type(1) {
-      width: 150px;
-      height: 150px;
-
-      position: relative;
-      left: 100px;
-      z-index: 0;
-
-      @media (pointer: fine) {
-        &:hover {
-          position: relative;
-
-          z-index: 2;
-          transform: scale(1.25);
-          bottom: 20px;
-          left: 95px;
-        }
-      }
+      grid-area: four;
+      justify-self: center;
     }
 
     :nth-of-type(2) {
-      width: 175px;
-      height: 175px;
-
-      position: relative;
-      left: 50px;
-      z-index: 1;
-
-      @media (pointer: fine) {
-        &:hover {
-          z-index: 3;
-          transform: scale(1.25);
-          bottom: 20px;
-          left: 45px;
-        }
-      }
+      grid-area: two;
+      justify-self: center;
     }
 
     :nth-of-type(3) {
-      width: 200px;
-      height: 200px;
-
-      position: relative;
-      z-index: 2;
-
-      @media (pointer: fine) {
-        &:hover {
-          z-index: 2;
-          transform: scale(1.25);
-          bottom: 20px;
-        }
-      }
+      grid-area: one;
+      margin-top: 50px;
+      justify-self: center;
     }
 
     :nth-of-type(4) {
-      width: 175px;
-      height: 175px;
-
-      position: relative;
-      right: 50px;
-      z-index: 1;
-
-      @media (pointer: fine) {
-        &:hover {
-          z-index: 2;
-          transform: scale(1.25);
-          right: 45px;
-          bottom: 20px;
-        }
-      }
+      grid-area: three;
+      justify-self: center;
     }
 
     :nth-of-type(5) {
-      width: 150px;
-      height: 150px;
-      position: relative;
-      right: 100px;
-      z-index: 0;
+      grid-area: five;
+      justify-self: center;
+    }
+  }
+`;
 
-      @media (pointer: fine) {
-        &:hover {
-          z-index: 2;
-          transform: scale(1.25);
-          right: 95px;
-          bottom: 20px;
-        }
-      }
+export const TouchScreenHeading = styled.h1`
+  @media (pointer: fine) {
+    display: none;
+  }
+
+  @media (pointer: coarse) {
+    display: revert;
+
+    grid-area: heading;
+
+    margin-top: 50px;
+    background-image: ${(props) => props.theme.colors.gradients.gold};
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -moz-background-clip: text;
+    -moz-text-fill-color: transparent;
+
+    justify-self: center;
+  }
+`;
+
+export const TouchScreenTextWrapper = styled.div`
+  @media (pointer: fine) {
+    display: none;
+  }
+
+  @media (pointer: coarse) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    margin-top: 10px;
+
+    h6 {
+      display: revert;
     }
   }
 `;
