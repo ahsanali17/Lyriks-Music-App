9b99a60646ee88689abac994e94c851613171f65
Joseph Ochoa <josephochoa91@gmail.com>
2022-10-15 14:02:59 -0700

    Make search be present in input after search

--- components/Search/Search.jsx
+++ components/Search/Search.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { BsSearch } from 'react-icons/bs';
 import { GiHamburgerMenu } from 'react-icons/gi';
-import {
-  SearchWrapper,
-  GradientWrapper,
-  IconWrapper,
-  MenuIconWrapper,
-} from './styles';
+
+import { SearchWrapper, GradientWrapper, IconWrapper, MenuIconWrapper } from './styles';
 import { setSearchQuery } from '../../redux/features/currentSongArtistList';
 
 const Search = () => {
   const dispatch = useDispatch();
+  const { searchQuery } = useSelector(state => state.currentSongArtistList);
   const [text, setText] = useState('');
 
   const handleInput = (e) => setText(e.target.value);
@@ -21,6 +18,10 @@ const Search = () => {
       dispatch(setSearchQuery(text));
     }
   };
+
+  useEffect(() => {
+    searchQuery ?  setText(searchQuery) : text;
+  }, [searchQuery])
 
   return (
     <SearchWrapper>
